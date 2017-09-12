092a27a159363d020a85c1723eaa4b0c36d74594
ericgreenwell <ericgreenwell2@gmail.com>
2017-09-12 14:35:37 -0400

    Update range.py
    add tesseract ocr

--- range.py
+++ range.py
@@ -1,6 +1,13 @@
 import picamera
+import image
+import tesseract
+# sudo apt-get install tesseract-ocr
 
-def range():
+def capture():
 	picamera.capture("range.jpg")
 	
-
+def measure():
+	img = Image.open("/home/pi/control/range.jpg")
+	cropped_img = img.crop() # img.crop(x1,y1,x2,y2) left and top start
+	range = pytesseract.image_to_string(cropped, config= ' outputbase digits')
+	print range
