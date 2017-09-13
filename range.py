3996570d5d757d4cda82b1c649188e338755edb3
ericgreenwell <ericgreenwell2@gmail.com>
2017-09-13 12:42:01 -0400

    uh oh

--- range.py
+++ range.py
@@ -1,13 +1,30 @@
-import picamera
-import image
-import tesseract
+#import picamera
+from PIL import Image, ImageEnhance, ImageFilter
+from pytesseract import image_to_string
+import time
+import cv2
+import os
+
 # sudo apt-get install tesseract-ocr
+# Python 3 file
 
 def capture():
 	picamera.capture("range.jpg")
 	
 def measure():
-	img = Image.open("/home/pi/control/range.jpg")
-	cropped_img = img.crop() # img.crop(x1,y1,x2,y2) left and top start
-	range = pytesseract.image_to_string(cropped, config= ' outputbase digits')
-	print range
+	
+#	image = cv2.imread('/home/eric/turret/image.jpg')#
+#	img.crop(x1,y1,x2,y2) left and top start
+
+	im = Image.open("image.jpg")
+	im = im.filter(ImageFilter.MedianFilter())
+	enhancer = ImageEnhance.Contrast(im)
+	im = enhancer.enhance(2)
+	im = im.convert("1")
+	im.save("new.jpg")
+	time.sleep(5)
+	range = image_to_string(Image.open("image.jpg"))
+	print(range)
+
+if __name__ == "__main__":
+	measure()
