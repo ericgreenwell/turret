9f21464a37ca9b0af7d4cd065314793c7dc771ed
ericgreenwell <ericgreenwell2@gmail.com>
2017-09-13 17:45:42 -0400

    second try image to string

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
-
 import pytesseract
+from pytesseract import image_to_string
 from PIL import Image, ImageEnhance, ImageFilter
 import cv2
-import numpy
+import numpy as np
 import imutils
 from imutils import contours
 
@@ -21,18 +21,7 @@ digits = {}
 
 
 
-# loop over training data and extract digits
-for (i,c) in enumerate(refCnts):
-	#compute bounding box
-	(x,y,w,h) = cv2.boundingRect(c)
-	roi = ref[y:w+h, x:x+w]
-	roi = cv2.resize(roi, (57,88))
-
-	#update digits dictionary
-	digits[i] = roi
-
-
-######## load image ###############33
+#load image ###############33
 
 image = cv2.imread("num.jpg")
 image = imutils.resize(image, width=300)
@@ -41,38 +30,6 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 #find contours 
 cnts = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 locs = []
-
-# loop over contours
-
-for (i,c) in enumerate(cnts):
-	(x,y,w,h) = cv2.boundingRect(c)
-	ar = w/float(h)
-	locs.append((x,y,w,h))
-
-locs = sorted(locs, key=lambda x:x[0])
-output = []
-
-digitCnts = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-digitCnts = digitCnts[0]
-digitCnts = contours.sort_contours(digitCnts, method="left-to-right")[0]
-
-#####loop again###
-
-for c in digitCnts:
-	(x,y,w,h) = cv2.boundingRect(c)
-	roi =gray[y:y+h, x:x+w]
-	roi = cv2.resize(roi, (57,88)) 
-	
-	scores = []
-
-	for (digit, digitROI) in digits.items():
-		result = cv2.matchTemplate(roi, digitROI,cv2.TM_CCOEFF)
-		(_, score, _, _) = cv2.minMaxLoc(result)
-		scores.append(score)
-
-print("range:{}".format(''.join(output)))
-cv2.imshow("image", image)
-
 """
 def getImage():
 	
@@ -87,25 +44,27 @@ def getImage():
 	cv2.imshow("blur", img3)
 	cv2.waitKey(0)
 
-
-
-
-def preprocessing(img):
-	pass	
-
 if __name__ == "__main__":
 	getImage()
-
+"""
 
 im = Image.open("num.jpg") # the second one 
 im = im.filter(ImageFilter.MedianFilter())
 enhancer = ImageEnhance.Contrast(im)
 im = enhancer.enhance(2)
 im = im.convert('1')
-im.save('temp2.jpg')
-text = pytesseract.image_to_string(Image.open('temp2.jpg'))
-print(text)
-"""
+kernel = np.ones((5,5), np.uint8)
+
+for i in range(10):
+	im = cv2.dilate(image,kernel, iterations = i)
+	cv2.imshow("dilation {}".format(i), im)
+	cv2.waitKey(0)
+	cv2.imwrite("test.jpg", im)
+	text = pytesseract.image_to_string(Image.open("test.jpg"))
+	print text
+#im.save('temp2.jpg')
+#text = pytesseract.image_to_string(Image.open('temp2.jpg'))
+#print(text)
 
 #https://stackoverflow.com/questions/17672705/text-detection-on-seven-segment-display-via-tesseract-ocr
 #https://stackoverflow.com/questions/30479002/digital-numbers-on-tesseract-ocr
