b0316d5ec5f9650b821eba3f2413ad9953613153
ericgreenwell <ericgreenwell2@gmail.com>
2017-09-20 21:53:31 -0400

    Update smctest.py

--- smctest.py
+++ smctest.py
@@ -1,6 +1,8 @@
+#Import pysmc
 from smc100 import *
 
-smc100 = SMC100(1, "/dev/tty5", silent=False)
+#create object on ttyUSB0 from dmesg | grep tty
+smc100 = SMC100(1, "/dev/ttyUSB0", silent=False)
 
 print smc100.get_position_mm()
 
@@ -13,7 +15,7 @@ assert smc100.get_status()[0] == 0
 pos = smc100.get_position_mm()
 
 smc.move_absolute_mm(5)
-
 assert smc100.get_status()[0] ==0
 
+# Clean Up
 del smc100
